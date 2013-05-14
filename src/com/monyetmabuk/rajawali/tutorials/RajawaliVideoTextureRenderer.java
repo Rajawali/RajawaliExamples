5e2bc4b31ea2bed51a46dfc140f93675c8dad15c
Dennis Ippel <ippeldv@gmail.com>
2013-05-14 20:14:56 +0100

    Added a video texture renderer example.

--- src/com/monyetmabuk/rajawali/tutorials/RajawaliVideoTextureRenderer.java
+++ src/com/monyetmabuk/rajawali/tutorials/RajawaliVideoTextureRenderer.java
@@ -4,23 +4,20 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import rajawali.BaseObject3D;
-import rajawali.animation.RotateAnimation3D;
 import rajawali.lights.DirectionalLight;
-import rajawali.materials.DiffuseMaterial;
+import rajawali.materials.VideoMaterial;
 import rajawali.materials.textures.ATexture.TextureException;
-import rajawali.materials.textures.Texture;
-import rajawali.math.Number3D.Axis;
+import rajawali.materials.textures.VideoTexture;
 import rajawali.primitives.Cube;
 import rajawali.renderer.RajawaliRenderer;
 import android.content.Context;
-import android.graphics.SurfaceTexture;
 import android.media.MediaPlayer;
 
 public class RajawaliVideoTextureRenderer extends RajawaliRenderer {
 	private BaseObject3D mCube;
-	private SurfaceTexture mSurfaceTexture;
 	private MediaPlayer mMediaPlayer;
-
+	private VideoTexture mVideoTexture;
+	
 	public RajawaliVideoTextureRenderer(Context context) {
 		super(context);
 		setFrameRate(60);
@@ -31,56 +28,51 @@ public class RajawaliVideoTextureRenderer extends RajawaliRenderer {
 		light.setColor(1.0f, 1.0f, 1.0f);
 		light.setPower(2);
 		
-		/*
-		 * 		VideoMaterial material = new VideoMaterial();
-		 * 		TextureInfo tInfo = mTextureManager.addVideoTexture();
-		 * 		
-		 * 		mTexture = new SurfaceTexture(tInfo.getTextureId());
-		 * 		
-		 * 		mMediaPlayer = MediaPlayer.create(getContext(), R.raw.nemo);
-		 * 		mMediaPlayer.setSurface(new Surface(mTexture));
-		 * 		mMediaPlayer.start();
-		 * 		
-		 * 		BaseObject3D cube = new Plane(2, 2, 1, 1);
-		 * 		cube.setMaterial(material);
-		 * 		cube.addTexture(tInfo);
-		 * 		cube.addLight(mLight);
-		 * 		addChild(cube);
-		 * 	}
-		 * 
-		 * 	public void onDrawFrame(GL10 glUnused) {
-		 * 		mTexture.updateTexImage();
-
-
+		mMediaPlayer = MediaPlayer.create(getContext(), R.raw.sintel_trailer_480p);
+		mMediaPlayer.setLooping(true);
+		
+		mVideoTexture = new VideoTexture("sintelTrailer", mMediaPlayer);
+		VideoMaterial material = new VideoMaterial();		
 		try {
-			DiffuseMaterial material = new DiffuseMaterial();
-			material.addTexture(new Texture(R.drawable.earthtruecolor_nasa_big));
-			mCube = new Cube(1);
-			mCube.setMaterial(material);
-			mCube.addLight(light);
-			addChild(mCube);
-
+			material.addTexture(mVideoTexture);
 		} catch (TextureException e) {
 			e.printStackTrace();
-		}
-*/
-		getCurrentCamera().setZ(4.2f);
+		}		
+		
+		mCube = new Cube(1);
+		mCube.setMaterial(material);
+		addChild(mCube);
+		 
+		getCurrentCamera().setZ(2.2f);
 	}
 
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		((RajawaliExampleActivity) mContext).showLoader();
 		super.onSurfaceCreated(gl, config);
 		((RajawaliExampleActivity) mContext).hideLoader();
+		mMediaPlayer.start();
 	}
 
 	public void onDrawFrame(GL10 glUnused) {
 		super.onDrawFrame(glUnused);
 		if (mCube != null) {
-			mCube.setRotX(mCube.getRotX() + .1f);
-			mCube.setRotZ(mCube.getRotZ() + .2f);
+			mCube.setRotX(mCube.getRotX() + .5f);
+			mCube.setRotZ(mCube.getRotZ() + .8f);
 		}
-		if(mSurfaceTexture != null) {
-			mSurfaceTexture.updateTexImage();
-		}
+		mVideoTexture.update();
+	}
+	
+	public void onVisibilityChanged(boolean visible) {
+		super.onVisibilityChanged(visible);
+		if(!visible)
+			if(mMediaPlayer != null) mMediaPlayer.pause();
+		else
+			if(mMediaPlayer != null) mMediaPlayer.start();
+	}
+	
+	public void onSurfaceDestroyed() {
+		super.onSurfaceDestroyed();
+		mMediaPlayer.stop();
+		mMediaPlayer.release();
 	}
 }
