025d21669327ba24396d3411cb2b3cd452728786
Jared Woolston <jwoolston@tenkiv.com>
2015-04-12 00:36:58 -0700

    Makes the view to texture demonstration more efficient.
    Signed-off-by: Jared Woolston <jwoolston@tenkiv.com>

--- examples/src/main/java/com/monyetmabuk/rajawali/tutorials/examples/ui/ViewToTextureFragment.java
+++ examples/src/main/java/com/monyetmabuk/rajawali/tutorials/examples/ui/ViewToTextureFragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 
 import com.monyetmabuk.rajawali.tutorials.R;
 import com.monyetmabuk.rajawali.tutorials.examples.AExampleFragment;
@@ -19,25 +20,33 @@ import com.monyetmabuk.rajawali.tutorials.examples.AExampleFragment;
 import org.rajawali3d.Object3D;
 import org.rajawali3d.animation.Animation;
 import org.rajawali3d.animation.RotateOnAxisAnimation;
-import org.rajawali3d.lights.DirectionalLight;
 import org.rajawali3d.materials.Material;
-import org.rajawali3d.materials.methods.DiffuseMethod;
 import org.rajawali3d.materials.textures.ATexture;
 import org.rajawali3d.materials.textures.Texture;
 import org.rajawali3d.math.vector.Vector3;
 import org.rajawali3d.primitives.Cube;
+import org.rajawali3d.util.RajLog;
 
 public class ViewToTextureFragment extends AExampleFragment {
 
-    CustomFragment mCustomFragment;
+    FragmentToDraw mFragmentToDraw;
     Handler mHandler;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         mHandler = new Handler(Looper.getMainLooper());
-        mCustomFragment = new CustomFragment();
-        getActivity().getSupportFragmentManager().beginTransaction().add(R.id.content_frame, mCustomFragment, "custom").commit();
+
+        final FrameLayout fragmentFrame = new FrameLayout(getActivity());
+        final FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(512, 512);
+        fragmentFrame.setLayoutParams(params);
+        fragmentFrame.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_bright));
+        fragmentFrame.setId(R.id.view_to_texture_frame);
+        fragmentFrame.setVisibility(View.INVISIBLE);
+        mLayout.addView(fragmentFrame);
+
+        mFragmentToDraw = new FragmentToDraw();
+        getActivity().getSupportFragmentManager().beginTransaction().add(R.id.view_to_texture_frame, mFragmentToDraw, "custom").commit();
         return mLayout;
     }
 
@@ -52,11 +61,11 @@ public class ViewToTextureFragment extends AExampleFragment {
     }
 
     private final class CanvasTextRenderer extends AExampleRenderer {
-        private Texture mTimeTexture;
-        private Bitmap mTimeBitmap;
-        private Canvas mTimeCanvas;
+        private Texture mViewTexture;
+        private Bitmap mViewBitmap;
+        private Canvas mViewCanvas;
         private int mFrameCount;
-        private boolean mShouldUpdateTexture;
+        private volatile boolean mShouldUpdateTexture;
 
         private Object3D mObject3D;
 
@@ -66,94 +75,77 @@ public class ViewToTextureFragment extends AExampleFragment {
 
         @Override
         public void initScene() {
-            DirectionalLight light = new DirectionalLight(.1f, .1f, -1);
-            light.setPower(2);
-            getCurrentScene().addLight(light);
+            mObject3D = new Cube(3.0f);
+            mObject3D.setColor(0);
 
-            Cube parentCube = null;
+            mObject3D.setPosition(0, 0, -3);
+            mObject3D.setRenderChildrenAsBatch(true);
 
-            for (int i = 0; i < 1; i++) {
-                Cube timeCube = new Cube(3.0f);
-                timeCube.setColor(0);
-
-                if (parentCube == null) {
-                    timeCube.setPosition(0, 0, -3);
-                    timeCube.setRenderChildrenAsBatch(true);
-                    parentCube = timeCube;
-                    mObject3D = timeCube;
-                } else {
-                    timeCube.setX(-3 + (float) (Math.random() * 6));
-                    timeCube.setY(-3 + (float) (Math.random() * 6));
-                    timeCube.setZ(-3 + (float) (Math.random() * 6));
-                    parentCube.addChild(timeCube);
-                }
-
-                int direction = Math.random() < .5 ? 1 : -1;
-
-                RotateOnAxisAnimation anim = new RotateOnAxisAnimation(Vector3.Axis.Y, 0,
-                    360 * direction);
-                anim.setRepeatMode(Animation.RepeatMode.INFINITE);
-                anim.setDurationMilliseconds(i == 0 ? 12000
-                    : 4000 + (int) (Math.random() * 4000));
-                anim.setTransformable3D(timeCube);
-                getCurrentScene().registerAnimation(anim);
-                anim.play();
+            Material viewMaterial = new Material();
+            viewMaterial.setColorInfluence(0);
+            mViewBitmap = Bitmap.createBitmap(mFragmentToDraw.getView().getWidth(),
+                mFragmentToDraw.getView().getHeight(), Bitmap.Config.ARGB_8888);
+            mViewTexture = new Texture("viewTexture", mViewBitmap);
+            try {
+                viewMaterial.addTexture(mViewTexture);
+            } catch (ATexture.TextureException e) {
+                e.printStackTrace();
             }
+
+            mViewCanvas = new Canvas(mViewBitmap);
+            mObject3D.setMaterial(viewMaterial);
+            getCurrentScene().addChild(mObject3D);
+
+            RotateOnAxisAnimation anim = new RotateOnAxisAnimation(Vector3.Axis.Y, 0,
+                360);
+            anim.setRepeatMode(Animation.RepeatMode.INFINITE);
+            anim.setDurationMilliseconds(12000);
+            anim.setTransformable3D(mObject3D);
+            getCurrentScene().registerAnimation(anim);
+            anim.play();
         }
 
-            final Runnable mUpdateTexture = new Runnable() {
-                public void run() {
-                    if (mTimeCanvas == null) {
-                        Material timeSphereMaterial = new Material();
-                        timeSphereMaterial.enableLighting(true);
-                        timeSphereMaterial.setDiffuseMethod(new DiffuseMethod.Lambert());
-                        timeSphereMaterial.setColorInfluence(0);
-                        mTimeBitmap = Bitmap.createBitmap(mCustomFragment.getView().getWidth(),
-                            mCustomFragment.getView().getHeight(), Bitmap.Config.ARGB_8888);
-                        mTimeTexture = new Texture("timeTexture", mTimeBitmap);
-                        try {
-                            timeSphereMaterial.addTexture(mTimeTexture);
-                        } catch (ATexture.TextureException e) {
-                            e.printStackTrace();
-                        }
-
-                        mTimeCanvas = new Canvas(mTimeBitmap);
-                        mObject3D.setMaterial(timeSphereMaterial);
-                        getCurrentScene().addChild(mObject3D);
-                    }
-                    // -- Draw the time on the canvas
-                    mCustomFragment.getView().draw(mTimeCanvas);
-                    // -- Indicates that the texture should be updated on the OpenGL thread.
-                    mShouldUpdateTexture = true;
-                }
-            };
+        final Runnable mUpdateTexture = new Runnable() {
+            public void run() {
+                // -- Draw the view on the canvas
+                mFragmentToDraw.getView().draw(mViewCanvas);
+                // -- Indicates that the texture should be updated on the OpenGL thread.
+                mShouldUpdateTexture = true;
+            }
+        };
 
         @Override
         protected void onRender(long ellapsedRealtime, double deltaTime) {
             // -- not a really accurate way of doing things but you get the point :)
-            if (mFrameCount++ >= 2*mFrameRate) {
+            if (mFrameCount++ >= 10) { //mFrameRate) {
                 mFrameCount = 0;
                 mHandler.post(mUpdateTexture);
             }
             // -- update the texture because it is ready
             if (mShouldUpdateTexture) {
-                mTimeTexture.setBitmap(mTimeBitmap);
-                mTextureManager.replaceTexture(mTimeTexture);
+                RajLog.i("Updating texture (" + ellapsedRealtime + ")");
+                mViewTexture.setBitmap(mViewBitmap);
+                mTextureManager.replaceTexture(mViewTexture);
                 mShouldUpdateTexture = false;
             }
             super.onRender(ellapsedRealtime, deltaTime);
         }
-
     }
 
-    public static final class CustomFragment extends Fragment {
+    public static final class FragmentToDraw extends Fragment {
+
+        WebView mWebView;
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             final View view = inflater.inflate(R.layout.view_to_texture, container, false);
-            final WebView webView = (WebView) view.findViewById(R.id.webview);
-            webView.setWebViewClient(new WebViewClient());
-            webView.loadUrl("http://plus.google.com/communities/116529974266844528013");
+            mWebView = (WebView) view.findViewById(R.id.webview);
+            mWebView.setWebViewClient(new WebViewClient());
+            // Load the Rajawali Repo commit activity graph
+            //webView.loadUrl("https://github.com/Rajawali/Rajawali/graphs/commit-activity");
+            mWebView.loadUrl("https://plus.google.com/communities/116529974266844528013");
+            mWebView.setInitialScale(50);
+            mWebView.setScrollY(0);
             view.setVisibility(View.INVISIBLE);
             return view;
         }
